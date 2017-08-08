65f222238f7e0cdc9a1886b0bf479dbdeda31573
BartPu <bpuchalski@tlen.pl>
2017-08-08 09:50:13 +0200

    commit scritps

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
-var a = prompt('Enter "a" value'),
-    b = prompt('Enter "b" value'),
-    value = (a * a) + (2 * a * b) - (b * b);
+var a = prompt('Podaj długość podstawy "a"'),
+    h = prompt('Podaj wysokość trójkąta "h"'),
+    triangleArea = (a * h) / 2,
+
+    function getTriangleArea(a, h) {
+        if (a > 0) || (h > 0) {
+            alert('Pole trójkąta wynosi: ' + triangleArea)
+            console.log(triangleArea)
+        } else {
+            alert('Nieprawidłowe dane.')
+        }
+    };
+
+
+
+
+value = (a * a) + (2 * a * b) - (b * b);
 
 alert('The result of (a * a) + (2 * a * b) - (b * b) operation is: ' + value);
 console.log(value);
@@ -15,3 +29,7 @@ if (value < 0) {
     alert('Wynik równy zero');
     console.log('Wynik równy zero');
 };
+
+
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
